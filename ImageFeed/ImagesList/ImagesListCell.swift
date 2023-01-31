44ec5e968f44a3b231b39c0dc6c8ed10dda2a6e1
Alexandr Sirotkin <aasirotkin@me.com>
2023-01-31 22:49:57 +0300

    Start doing table view first cell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Александр Сироткин on 31.01.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell : UITableViewCell {
+
+    static let reuseIdentifier = "ImagesListCell"
+
+}
