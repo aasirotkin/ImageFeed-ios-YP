c68329e0be75d911e771a7b0c09a67b06c1614cb
Alexandr Sirotkin <aasirotkin@me.com>
2023-02-06 20:36:13 +0300

    Start doing profile view

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Сироткин on 06.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController : UIViewController {
+
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var quitButton: UIButton!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var aboutLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
