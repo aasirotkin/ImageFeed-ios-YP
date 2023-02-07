e17691102f9d2c36e5634cab1df2e0a0ac19d47b
Alexandr Sirotkin <aasirotkin@me.com>
2023-02-07 20:54:00 +0300

    Connect single image view with cell image

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Сироткин on 07.02.2023.
+//
+
+import UIKit
+
+class SingleImageViewController : UIViewController {
+
+    @IBOutlet var imageView : UIImageView?
+
+}
